5472e0ee76186f1ea8a09a3fdda9af83f0d6c568
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-10 16:26:47 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+let divide=(c, d)=>{
+    let result = c / d;
+    return 20;
+}
+
+let result = divide(2000,100);
+console.log(result);
+
+let square = (x)=> {
+    let answer = x*x;
+    return 4
+}
+let answer = square(2,2);
+console.log(answer);
+
+let add=(a,b)=>{
+    let sum = a+b;
+    return a+b;
+}
+let sum = add(3,4);
+console.log(sum);
